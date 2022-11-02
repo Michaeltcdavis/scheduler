f9bdabaecd8ff7108eed98ed23a8c851df2c6b63
Michael Davis <michaeltcdavis@gmail.com>
2022-11-02 09:57:53 -0400

    incorporate components into application.js and refactor

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DayListItem from './DayListItem';
+
+export default function DayList(props) {
+  return props.days.map((day) => {
+  return (
+    <ul>
+      <DayListItem
+        key={day.id}
+        spots={day.spots}
+        name={day.name}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    </ul>
+    )
+  })
+};
---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classNames from "classnames";
+import './DayListItem.scss'
+
+
+export default function DayListItem(props) {
+  const formatSpots = function(spotsNo) {
+    let spotsPhrase = ''
+    if (spotsNo > 1) {
+      spotsPhrase += `${spotsNo} spots`
+    } 
+    if (spotsNo === 1) {
+      spotsPhrase += `${spotsNo} spot`
+    }
+    if (spotsNo < 1) {
+      spotsPhrase += 'no spots'
+    }
+    spotsPhrase += ' remaining'
+    return spotsPhrase;
+  }
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots 
+  })
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
