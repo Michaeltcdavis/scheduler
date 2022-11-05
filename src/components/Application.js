1906e1f1515abb8fba9cb73e5523382bc991eaa4
Michael Davis <michaeltcdavis@gmail.com>
2022-11-05 16:23:42 -0400

    add getInstructorsForDay function to pass instructors array to appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,68 +1,50 @@
+
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 import DayList from "./DayList";
-import Appointment from "components/Appointment/index.js"
+import Appointment from "components/Appointment"
 
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import "components/Application.scss";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+export default function Application() {
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
 
-const appointmentsArr = Object.values(appointments).map((appointment) => (
-  <Appointment
-    key={appointment.id}
-    {...appointment}
-  />
-));
+  const appointments = getAppointmentsForDay(state, state.day);
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    const interviewers = getInterviewersForDay(state, state.day);
 
-export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState(['Monday']);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        student={appointment.student}
+        interview={interview}
+        interviewers={interviewers}
+      />
+    )
+  });
+  const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
-    axios.get('/api/days')
-      .then(res => {
-        console.log(res);
-        setDays([...res.data]);
-    })
-  }, [])
+    let ignore = false;
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    });
+    return () => ignore = true;
+  }, []); 
 
   return (
     <main className="layout">
@@ -75,8 +57,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
@@ -87,8 +69,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentsArr}
+        {schedule}
       </section>
     </main>
   );
 }
+
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,30 @@
-import React, { Fragmant } from 'react';
+import React from 'react';
 import './styles.scss';
-import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+
+import useVisualMode from "hooks/useVisualMode";
+
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const STATUS = "STATUS";
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <header>{props.time}</header>
-      {props.interview ?
-        <Show interviewer={props.interview.interviewer} student={props.interview.student} />
-        : <Empty onAdd={props.onAdd} />
-      }
-    </article>
-  );
+      {mode === SHOW && <Show interviewer={props.interview.interviewer} student={props.interview.student} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE &&
+        <Form
+        interviewers={props.interviewers}
+        onSave={() => transition(STATUS)}
+        onCancel={() => back(EMPTY)}
+        />}
+      </article>
+      );
 };
